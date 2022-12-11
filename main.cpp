a4d386921a9bf30325d81324b8199af4a3b5efa7
Nishan Sivakumar <n22sivak@uwaterloo.ca>
2022-12-10 20:02:47 -0500

    final changes

--- main.cpp
+++ main.cpp
@@ -14,9 +14,8 @@ int main (void){
         cout << "1.View all employees\n";
         cout << "2.Add an employee.\n";
         cout << "3.Delete an employee.\n";
-        cout << "4.Edit an employee.\n";
-        cout << "5.Get Employee by ID\n";
-        cout << "6.Exit\n";
+        cout << "4.Get Employee by ID\n";
+        cout << "5.Exit\n";
         cout << "Enter an input: "; 
         cin >> input;
             if(input == 1){
@@ -32,6 +31,7 @@ int main (void){
                         break;
                     }
                     cout << "Employee Name: " << temp.readName() << "\n";
+                    cout << "Employee Number: " << temp.employeeNumber << "\n";
                     cout << "Employee Title: " << temp.readTitle() << "\n";
                     cout << "Employee Department: " << temp.readDepartment() << "\n";
                     cout << "**********************\n";
@@ -85,8 +85,6 @@ int main (void){
                 ofstream file_obj;
                 file_obj.open("Employees.txt", ios::app);
                 file_obj.write((char*)&temp, sizeof(temp));
-                string newline = "\n";
-                file_obj.write((char*)&newline, sizeof(newline));
             }
             else if(input == 3){
                 int empIdInput;
@@ -96,67 +94,31 @@ int main (void){
                 Employee temp;
                 ifstream file_obj;
                 file_obj.open("Employees.txt", ios::in);
+                ofstream old_file_obj;
+                old_file_obj.open("OldEmps.txt");
                 while(!file_obj.eof()){
                     file_obj.read((char*)&temp, sizeof(temp));
-                    if(temp.employeeNumber == empIdInput){
-                        
+                    if(!(temp.employeeNumber == empIdInput)){
+                        old_file_obj.write((char*)&temp, sizeof(temp));
                     }
                 }
-                
+                file_obj.close();
+                old_file_obj.close();
+
+                ifstream oldx_file_obj;
+                oldx_file_obj.open("OldEmps.txt", ios::in);
+                ofstream new_file_obj;
+                new_file_obj.open("Employees.txt");
+                while(!oldx_file_obj.eof()){
+                    temp.employeeNumber = 111;
+                    oldx_file_obj.read((char*)&temp, sizeof(temp));
+                       if(temp.employeeNumber == 111){
+                        break;
+                    }
+                    new_file_obj.write((char*)&temp, sizeof(temp));
+                }   
             }
             else if(input == 4){
-                int empIdInput;
-                cout << "Enter Employee Number: ";
-                cin >> empIdInput; 
-                int income;
-                char role[50];
-                char title[50];
-                char department [50];
-                char firstName [50];
-                char lastName [50];
-                bool active = true;
-
-                cout << "Enter Income :";
-                cin >> income;
-
-                cout << "Enter Role: ";
-                cin >> role;
-
-                cout << "Enter Title :";
-                cin >> title;
-
-                cout << "Enter Department: ";
-                cin >> department;
-
-                cout << "Enter First Name";
-                cin >> firstName;
-
-                cout << "Enter Last Name";
-                cin >> lastName;
-
-                ifstream file_obj;
-                file_obj.open("Employees.txt", ios::in);
-                while(!file_obj.eof() && active){
-                    Employee temp;
-                    file_obj.read((char*)&temp, sizeof(temp));
-                    if(temp.employeeNumber == empIdInput){
-                        string line;
-                        getline(file_obj, line);
-                        cout << line;
-                        temp.setIncome(income);
-                        temp.employeeNumber = empIdInput;
-                        temp.setRole(role);
-                        temp.setTitle(title);
-                        temp.setDepartment(department);
-                        temp.setName(firstName, lastName);
-                        ofstream file_obj;
-                        file_obj.open("Employees.txt", ios::app);
-                        file_obj.write((char*)&temp, sizeof(temp));
-                        active = false;
-                    }
-                }
-            }
-            else if(input == 5){
                 int empIdInput;
                 cout << "Enter Employee Number: ";
                 cin >> empIdInput; 
